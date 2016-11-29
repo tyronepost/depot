04e5530980a481525ec9d76420afba65dd2b8554
tyronepost <tyrone.post.a@gmail.com>
2016-11-29 07:36:57 -0500

    adding store controller and view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: {
     with:   %r{\.(gif|jpg|png)\Z}i,
