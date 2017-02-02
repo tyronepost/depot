16710f1ad07f3040dc78e254950483597c3f4bc3
tyronepost <tyrone.post.a@gmail.com>
2017-02-01 20:58:08 -0500

    flushing out email templates

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,10 +6,10 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,9 +17,9 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped
-    @greeting = "Hi"
+  def shipped(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 end
