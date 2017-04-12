1202f1f331307061892159a52542f6b063749b5d
tyronepost <tyrone.post.a@gmail.com>
2017-04-12 19:48:33 -0400

    fixed mailer tests

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,5 @@
 class OrderMailer < ApplicationMailer
-  default from: 'Tyrone Post <tyrone.post.a@gmail.com>'
+  default from: 'Sam Ruby <depot@example.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
