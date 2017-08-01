b2de551aa36eb303278b600ef30f35603e84e2c8
tyronepost <tyrone.post.a@gmail.com>
2017-08-01 17:50:53 -0400

    restarting depot app from scratch

--- app/controllers/store_controller.rb
+++
@@ -1,7 +0,0 @@
-class StoreController < ApplicationController
-  include CurrentCart
-  before_action :set_cart
-  def index
-    @products = Product.order(:title)
-  end
-end
--- db/migrate/20161229000025_add_order_to_line_item.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderToLineItem < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :line_items, :order, foreign_key: true
-  end
-end
