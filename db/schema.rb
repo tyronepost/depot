9e5c10ebc522ba814ce94ea3bc0580625d4227c2
tyronepost <tyrone.post.a@gmail.com>
2017-09-03 13:47:34 -0400

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170902152136) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+=======
+ActiveRecord::Schema.define(version: 20170819121338) do
+>>>>>>> parent of 54cd9a0... removed cart code from application.html.erb to fix tests
 
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
