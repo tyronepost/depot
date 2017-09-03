e957860550d4fa0c62ebd82fd5703990dfaa34eb
tyronepost <tyrone.post.a@gmail.com>
2017-09-03 13:53:32 -0400

    adding line_items resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
 
   resources :products
+  resources :line_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
