05612fa5627442457b01e4028ee17bfbfa31dbce
tyronepost <tyrone.post.a@gmail.com>
2017-09-07 10:50:56 -0400

    Iteration E2: Handling Errors

--- db/migrate/20170905213809_combine_items_in_cart.rb
+++ db/migrate/20170905213809_combine_items_in_cart.rb
@@ -19,9 +19,9 @@ class CombineItemsInCart < ActiveRecord::Migration[5.0]
     LineItem.where("quantity>1").each do |line_item|
       line_item.quantity.times do
         LineItem.create(
-                 cart_id: line_item.cart_id,
-                 product_id: line_item.product_id,
-                 quantity: 1
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
         )
       end
       line_item.destroy
